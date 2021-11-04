c49acfc0483c03bfff9b9d20375bdf28919d0fea
taimaahamarneh <taimaahamarneh1@gmail.com>
2021-11-04 09:20:28 +0200

    make the card take the dat loop

--- src/app/about/aboutus/aboutus.component.ts
+++ src/app/about/aboutus/aboutus.component.ts
@@ -8,20 +8,21 @@ import { Router } from '@angular/router';
 })
 export class AboutusComponent implements OnInit {
   data :any =[{
-    typeLang:"Back End",
+    typeLang:"who we are",
       subText:"C#" ,
-      description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. C# enables developers to build many types of secure and robust applications that run in .NET. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers. This tour provides an overview of the major components of the language in C# 8 and earlier. If you want to explore the language through interactive examples, try the introduction to C# tutorials."
-  },
+      description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "  },
   {
-    typeLang:"Front End" ,
+    typeLang:"our polices" ,
      subText:"Angular" ,
-     description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. C# enables developers to build many types of secure and robust applications that run in .NET. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers. This tour provides an overview of the major components of the language in C# 8 and earlier. If you want to explore the language through interactive examples, try the introduction to C# tutorials."
+     description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "
   },
   {
+    typeLang:"team work",
+    subText:"OOP",
+    description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "  },  {
     typeLang:"Back End",
     subText:"OOP",
-    description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. C# enables developers to build many types of secure and robust applications that run in .NET. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers. This tour provides an overview of the major components of the language in C# 8 and earlier. If you want to explore the language through interactive examples, try the introduction to C# tutorials."
-  }
+    description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "  }
   
   ]  
   constructor(private router:Router) { 
