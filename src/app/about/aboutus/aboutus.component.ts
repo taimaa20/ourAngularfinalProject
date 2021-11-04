4b0c3dfae957ae63587b710d200ae243e9890766
taimaahamarneh <taimaahamarneh1@gmail.com>
2021-11-05 01:03:32 +0200

    finish about us

--- src/app/about/aboutus/aboutus.component.ts
+++ src/app/about/aboutus/aboutus.component.ts
@@ -8,18 +8,23 @@ import { Router } from '@angular/router';
 })
 export class AboutusComponent implements OnInit {
   data :any =[{
+    img:"who we are",
     typeLang:"who we are",
       subText:"C#" ,
       description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "  },
   {
+    img:"who we are",
     typeLang:"our polices" ,
      subText:"Angular" ,
      description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "
   },
   {
+    img:"who we are",
     typeLang:"team work",
     subText:"OOP",
-    description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "  },  {
+    description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "  },
+      {
+        img:"who we are",
     typeLang:"Back End",
     subText:"OOP",
     description:"C# See Sharp is a modern, object-oriented, and type-safe programming language. "  }
