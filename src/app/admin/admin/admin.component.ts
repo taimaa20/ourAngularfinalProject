a7f097a91d08f25f6cf96dbb698072150bc17fe4
THORAYYA BANY YASIN <tsbanyyasin15@cit.just.edu.jo>
2021-11-10 03:16:42 +0200

    hitapi

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { HomeService } from 'src/app/Service/home.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,18 +10,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
-  constructor(private router:Router) { }
+  @Input () number_Of_Users:number|undefined;
+  constructor(private router:Router,public homeService : HomeService,
+    public tostr:ToastrService,
+    private spiner:NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.GetNumberOfCustomer();
   }
   logout()
   {
     localStorage.clear();
     this.router.navigate(['security/login']);
   }
-  Dashboard()
+  GetNumberOfCustomer()
   {
-    
+    this.spiner.show();
+
+this.homeService.GetNumberOfCustomer().subscribe((res:any)=>{
+  this.homeService.data=res;
+  console.log(this.homeService.data.number_Of_Users)
+  this.spiner.hide();
+  this.tostr.success('Data Retrived !!!')
+},err=>{
+  this.spiner.hide();
+  this.tostr.error('something want worring!!')
+})
   }
 }
